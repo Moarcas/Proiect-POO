878ff2b902f4ed1b89f80a484867acedc6c64377
Moarcas Cosmin-Ionut <cosminmoarcas01@gmail.com>
2022-04-13 11:19:52 +0300

    Modificari

--- main.cpp
+++ main.cpp
@@ -67,7 +67,7 @@ class Persoana {
     std::string nume;
     std::vector<Masa> m;
     double inaltime, greutate;
-    int calorii_arse, calorii_mancare;
+    //int calorii_arse, calorii_mancare;
 
 public:
     Persoana(const std::string& nume_, const std::vector<Masa>& m_, double inaltime_, double greutate_) : nume{nume_}, m{m_}, inaltime{inaltime_}, greutate{greutate_} {
@@ -89,6 +89,8 @@ public:
         greutate = other.greutate;
 
         std::cout << "operator= copiere Persoana\n";
+
+        return *this;
     }
 
     ~Persoana() {
@@ -120,16 +122,14 @@ public:
             else
                 std::cout << nume << "'s Body Mass Index is " << bmi << ". This is considered normal.\n";
     }
-
-
 };
 
-class Exercitiu {
-    std::string nume;
-    int calorii_arse;
-public:
-
-};
+// class Exercitiu {
+//     std::string nume;
+//     int calorii_arse;
+// public:
+//
+// };
 
 
 int main() {
